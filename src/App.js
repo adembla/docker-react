0b8364524071a07873ee87f09055a72cd83ecabe
anand dembla <aanand.dembla@gmail.com>
2019-12-28 13:17:22 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in 2 days
         </a>
       </header>
     </div>
